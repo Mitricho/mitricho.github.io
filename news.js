7a95cd4a2c51adcc42deb5e9c4b49de952821d7c
DevelopRepo <philonenko@gmail.com>
2023-05-09 23:40:49 +0500

    Create news.js

---
+++ news.js
@@ -0,0 +1,22 @@
+var news = [
+  {
+    title:"Marjorie Taylor Greene",
+    image:"https://media1.faz.net/ppmedia/aktuell/3171325626/1.8846631/amtseinfuehrung-joe-biden-wird.jpg",
+    text:"Marjorie Taylor Greene claims Biden a criminal 'mastermind' after calling him 'mentally incompetent'"
+  },
+  {
+    title:"Red Roses for Mothers Day",
+    image:"https://m.media-amazon.com/images/I/71zNWbTHzxL.jpg",
+    text:"Red Roses for Mothers Day- 50 Fresh Cut Flowers- Beautiful Gift"
+  },  
+  {
+    title:"White Flowers for Your Garden",
+    image:"https://www.thespruce.com/thmb/6KsyQ85ioELI6CE3WsKcx85LGvs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/white-flowers-for-your-garden-4122501-01-54dac903a41b4244951ac801f7f0d5b9.JPG",
+    text:"White flowers take the guesswork out of a garden design. Shades of cream, ivory, silver, and pure snow blossoms layer seamlessly to form a peaceful landscape that glows in the evening. White flowers for fragrance, for cutting, and for wildlife will make your flower garden a respite, no matter your climate. "
+  },  
+  {
+    title:"Bring On The Happy Vase of Flowers in Elizabethtown, KY - ELIZABETHTOWN FLORIST & GREENHOUSE",
+    image:"https://cdn.atwilltech.com/flowerdatabase/b/bring-on-the-happy-vase-of-flowers-VA02512.425.jpg",
+    text:"Your flowers will be hand picked on the day by one of our expert florists, hand tied together and presented in either a gift box or a glass vase."
+  },   
+];
