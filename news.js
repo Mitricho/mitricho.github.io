2e010a8e39cdc7581741d0c0c5c8c98bf8e77264
DevelopRepo <philonenko@gmail.com>
2023-05-17 08:03:07 +0500

    Update news.js

--- news.js
+++ news.js
@@ -2,21 +2,25 @@ var news = [
   {
     title:"sushi",
     image:"https://img.freepik.com/free-photo/maki-sushi-isolated-white_2829-5653.jpg?w=1060&t=st=1684247641~exp=1684248241~hmac=dac897dfe25e6895fcd387b7f33b6441b5ebae3aa00f8af2094183ab1b747546",
-    text:"Sushi is a Japanese dish of prepared vinegared rice (鮨飯, sushi-meshi), usually with some sugar and salt, accompanied by a variety of ingredients"
+    text:"Sushi is a Japanese dish of prepared vinegared rice (鮨飯, sushi-meshi), usually with some sugar and salt, accompanied by a variety of ingredients",
+    price:295
   },
   {
     title:"Mixed sushi set",
     image:"https://img.freepik.com/free-photo/mixed-sushi-set-japanese-food_1339-3610.jpg?w=1800&t=st=1684247742~exp=1684248342~hmac=8b4f8adfa8e750e788ce0d50a757c910dd98bba4f969da78ab861c465279f88d",
-    text:"Red Roses for Mothers Day- 50 Fresh Cut Flowers- Beautiful Gift"
+    text:"Red Roses for Mothers Day- 50 Fresh Cut Flowers- Beautiful Gift",
+    price:499
   },  
   {
     title:"Salmon sashimi",
     image:"https://img.freepik.com/free-photo/japanese-food_1203-6844.jpg?w=1480&t=st=1684247843~exp=1684248443~hmac=4480679876ab75410bce03730393dadd9beaa371d132c89e4e0b6a79df67d90b",
-    text:"Soy sauce, ponzu sauce, wasabi, pickled ginger, lemon"
+    text:"Soy sauce, ponzu sauce, wasabi, pickled ginger, lemon",
+    price:379
   },  
   {
     title:"Sushi Nigiri",
     image:"https://img.freepik.com/premium-photo/group-rolls-maki-isolated-white-background_185193-73954.jpg?w=1480",
-    text:"Nigiri sushi is that familiar style of sushi made up of an oval-shaped mound of rice with a slice of (usually) raw fish on top."
+    text:"Nigiri sushi is that familiar style of sushi made up of an oval-shaped mound of rice with a slice of (usually) raw fish on top",
+    price:495
   }
 ];
